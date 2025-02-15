ff6d0c60b3ba0033772a425b4cfe436d81b7b21f
aapsi <aapsikhaira98@gmail.com>
2025-02-15 13:23:36 +0530

    feat: update submodule commits and add Raffle contract implementation

---
+++ src/Raffle.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title Raffle
+ * @author Aapsi Khaira
+ * @dev A contract for a raffle which implements chainlink VRF v2.5
+ * @notice This contract is for creating a raffle
+ */
+contract Raffle{
+
+    // errors
+    error Raffle_SendMoreToEnterRaffle();
+
+    uint256 private immutable i_entryFee;
+    
+    address payable[] private s_players;
+
+    // events
+    event RaffleEntered(address indexed player, uint256 amount);
+
+
+    constructor(uint256 entryFees) {
+        i_entryFee = entryFees;
+    }
+
+    function enterRaffle() public payable{
+        if(msg.value != i_entryFee){
+            revert Raffle_SendMoreToEnterRaffle();
+        }
+
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender, msg.value);
+
+    }
+
+    function pickWinner() public {
+        
+
+    }
+
+
+    /**
+     * @dev Returns the entry fee
+     */
+    function getEntryFee() public view returns(uint256){
+        return i_entryFee;
+    }
+}
